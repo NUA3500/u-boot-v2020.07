2ed1d7ec9593248bf0efda4944540f15490b6d4c
mjchen <mjchen@nuvoton.com>
2020-09-29 17:26:19 +0800

    update uart driver
    Change-Id: Ia50db84948342d1b033f333296ff93a23e4a1adc

--- drivers/serial/serial_nuvoton.c
+++ drivers/serial/serial_nuvoton.c
@@ -9,6 +9,7 @@
 #include <common.h>
 #include <div64.h>
 #include <dm.h>
+#include <dt-structs.h>
 #include <errno.h>
 #include <log.h>
 #include <serial.h>
@@ -16,17 +17,14 @@
 #include <asm/io.h>
 #include <asm/types.h>
 #include <linux/err.h>
-
+#include <dm/device_compat.h>
+#include <debug_uart.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
-void nua3500_serial_initialize(void);
-int nua3500_serial_init (void);
-void nua3500_serial_putc (const char ch);
-void nua3500_serial_puts (const char *s);
-int nua3500_serial_getc (void);
-int nua3500_serial_tstc (void);
-void nua3500_serial_setbrg (void);
+static int nua3500_serial_putc(struct udevice *dev, const char ch);
+static int nua3500_serial_getc(struct udevice *dev);
+static int nua3500_serial_setbrg(struct udevice *dev, int baudrate);
 
 #define TX_RX_FIFO_RESET    0x06
 #define ENABLE_DLAB         0x80    // Enable Divider Latch Access
@@ -35,108 +33,129 @@ void nua3500_serial_setbrg (void);
 #define THR_EMPTY           0x20    // Transmit Holding Register Empty
 #define RX_DATA_READY       0x01
 #define RX_FIFO_EMPTY       0x4000
+#define TX_FIFO_EMPTY       0x400000
+#define TX_FIFO_FULL        0x800000
 
-#define REG_COM_MSR    (0x18)   /* (R) Modem status register */
+struct nua3500_serial_regs {
+	volatile u32 RBR_THR;
+	volatile u32 IER;
+	volatile u32 FCR;
+	volatile u32 LCR;
+	volatile u32 MCR;
+	volatile u32 MSR;
+	volatile u32 FSR;
+	volatile u32 ISR;
+	volatile u32 TOR;
+	volatile u32 BAUD;
+	volatile u32 IRCR;
+	volatile u32 ALTCON;
+	volatile u32 FUNSEL;
+};
 
-typedef struct {
-	union {
-		volatile unsigned int RBR;      /*!< Offset: 0x0000   Receive Buffer Register           */
-		volatile unsigned int THR;      /*!< Offset: 0x0000   Transmit Holding Register         */
-	} x;
-	volatile unsigned int IER;              /*!< Offset: 0x0004   Interrupt Enable Register         */
-	volatile unsigned int FCR;              /*!< Offset: 0x0008   FIFO Control Register             */
-	volatile unsigned int LCR;              /*!< Offset: 0x000C   Line Control Register             */
-	volatile unsigned int MCR;              /*!< Offset: 0x0010   Modem Control Register            */
-	volatile unsigned int MSR;              /*!< Offset: 0x0014   Modem Status Register             */
-	volatile unsigned int FSR;              /*!< Offset: 0x0018   FIFO Status Register              */
-	volatile unsigned int ISR;              /*!< Offset: 0x001C   Interrupt Status Register         */
-	volatile unsigned int TOR;              /*!< Offset: 0x0020   Time Out Register                 */
-	volatile unsigned int BAUD;             /*!< Offset: 0x0024   Baud Rate Divisor Register        */
-	volatile unsigned int IRCR;             /*!< Offset: 0x0028   IrDA Control Register             */
-	volatile unsigned int ALTCON;           /*!< Offset: 0x002C   Alternate Control/Status Register */
-	volatile unsigned int FUNSEL;           /*!< Offset: 0x0030   Function Select Register          */
-} UART_TypeDef;
+struct nua3500_serial_platdata {
+	unsigned long base;
+	unsigned int clock;
+};
 
-#define SYS_GPE_MFPH	0x404600A4
-#define UART0_BASE	0x40700000 /* UART0 Control (High-Speed UART) */
-#define UART0           ((UART_TypeDef *)UART0_BASE)
-/*
- * Initialise the serial port with the given baudrate. The settings are always 8n1.
- */
+struct nua3500_serial_priv {
+	struct nua3500_serial_regs *regs;
+};
 
-
-int nua3500_serial_init (void)
+static int nua3500_serial_putc(struct udevice *dev, const char ch)
 {
-	__raw_writel(__raw_readl(0X4046020C) | (1 << 12), 0X4046020C);  // Uart CLK @ APBCLK0
-	__raw_writel(__raw_readl(0X40460220) & ~(3 << 16), 0X40460220);      // Uart CLK from HXT
+	struct nua3500_serial_priv *priv = dev_get_priv(dev);
+	struct nua3500_serial_regs *regs = priv->regs;
 
-	/* UART0 line configuration for (9600,n,8,1) */
-	UART0->LCR |=0x07;
-	UART0->BAUD = 0x3000000E;       /* 24MHz reference clock input, 9600 */
-	UART0->FCR |=0x02;              // Reset UART0 Rx FIFO
+	if(regs->FSR & TX_FIFO_FULL)
+		return -EAGAIN;
+	else
+		regs->RBR_THR = ch;
 
-	__raw_writel((__raw_readl(SYS_GPE_MFPH) & ~0xff000000) | 0x11000000, SYS_GPE_MFPH); // UART0 multi-function
-
-	printf("NUA3500 UART ok\n");
 	return 0;
 }
 
-void nua3500_serial_putc (const char ch)
+static int nua3500_serial_pending(struct udevice *dev, bool input)
 {
-	while ((UART0->FSR & 0x800000)); //waits for TX_FULL bit is clear
-	UART0->x.THR = ch;
-	if(ch == '\n') {
-		while((UART0->FSR & 0x800000)); //waits for TX_FULL bit is clear
-		UART0->x.THR = '\r';
+	struct nua3500_serial_priv *priv = dev_get_priv(dev);
+	struct nua3500_serial_regs *regs = priv->regs;
+	unsigned int lsr;
+
+	lsr = regs->FSR;
+
+	if (input) {
+		return (lsr & RX_FIFO_EMPTY) ? 0 : 1;
+	} else {
+		return (lsr & TX_FIFO_EMPTY) ? 0 : 1;
 	}
 }
 
-void nua3500_serial_puts (const char *s)
+static int nua3500_serial_getc(struct udevice *dev)
 {
-	while (*s) {
-		nua3500_serial_putc (*s++);
-	}
+	struct nua3500_serial_priv *priv = dev_get_priv(dev);
+	struct nua3500_serial_regs *regs = priv->regs;
+
+	if (!(regs->FSR & RX_FIFO_EMPTY))
+		return (regs->RBR_THR);
+	else
+		return -EAGAIN;
 }
 
-int nua3500_serial_getc (void)
+static int nua3500_serial_setbrg(struct udevice *dev, int baudrate)
 {
-	while (1) {
-		if (!(UART0->FSR & (1 << 14))) {
-			return (UART0->x.RBR);
-		}
-		//WATCHDOG_RESET();
-	}
+	struct nua3500_serial_priv *priv = dev_get_priv(dev);
+	struct nua3500_serial_regs *regs = priv->regs;
+
+	// Set for emulation
+	regs->LCR |=0x07;
+	regs->BAUD = 0x3000000E;
+	regs->FCR |=0x02;
+
+	return 0;
 }
 
-int nua3500_serial_tstc (void)
-{
-	return (!((__raw_readl(UART0_BASE + REG_COM_MSR) & RX_FIFO_EMPTY)>>14));
-}
-
-void nua3500_serial_setbrg (void)
-{
-
-	return;
-}
-
-/* TODO : CWWeng 2020.8.31 : replace this driver with driver model */
-static struct serial_device nua3500_serial_drv = {
-	.name   = "nua3500_serial",
-	.start  = nua3500_serial_init,
-	.stop   = NULL,
+static const struct dm_serial_ops nua3500_serial_ops = {
+	.putc = nua3500_serial_putc,
+	.pending = nua3500_serial_pending,
+	.getc = nua3500_serial_getc,
 	.setbrg = nua3500_serial_setbrg,
-	.putc   = nua3500_serial_putc,
-	.puts   = nua3500_serial_puts,
-	.getc   = nua3500_serial_getc,
-	.tstc   = nua3500_serial_tstc,
 };
 
-void nua3500_serial_initialize(void)
+static int nua3500_serial_probe(struct udevice *dev)
 {
-	serial_register(&nua3500_serial_drv);
+	struct nua3500_serial_platdata *plat = dev_get_platdata(dev);
+	struct nua3500_serial_priv *priv = dev_get_priv(dev);
+	fdt_addr_t addr;
+
+	addr = devfdt_get_addr(dev);
+
+	if (addr == FDT_ADDR_T_NONE)
+		return -EINVAL;
+
+	plat->base = addr;
+	priv->regs = (struct nua3500_serial_regs *)plat->base;
+
+	//enable clock
+	__raw_writel(__raw_readl(0X4046020C) | (0x1 << 12), 0X4046020C);
+	__raw_writel(__raw_readl(0X40460220) & ~(3 << 16), 0X40460220);
+
+	return 0;
 }
 
-__weak struct serial_device *default_serial_console(void) {
-	return &nua3500_serial_drv;
-}
+static const struct udevice_id nua3500_serial_id[] = {
+	{.compatible = "nuvoton,nua3500-uart"},
+	{}
+};
+
+U_BOOT_DRIVER(serial_nua3500) = {
+	.name = "serial_nua3500",
+	.id = UCLASS_SERIAL,
+	.of_match = nua3500_serial_id,
+	.platdata_auto_alloc_size = sizeof(struct nua3500_serial_platdata),
+	.probe = nua3500_serial_probe,
+	.ops = &nua3500_serial_ops,
+	.priv_auto_alloc_size = sizeof(struct nua3500_serial_priv),
+	.flags = DM_FLAG_PRE_RELOC,
+};
+
+
 
